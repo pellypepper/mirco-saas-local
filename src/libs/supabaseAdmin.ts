d3a5b41edbfad1b9d14af9a32e741807794c2fec
ppeliance <144333180+ppeliance@users.noreply.github.com>
2026-04-20 19:05:56 +0100

    cookies changes

--- src/libs/supabaseAdmin.ts
+++ src/libs/supabaseAdmin.ts
@@ -1,7 +1,24 @@
-import { createClient } from '@supabase/supabase-js';
+// lib/supabase/server.ts
+import { createServerClient } from '@supabase/ssr';
+import { cookies } from 'next/headers';
 
-export const supabaseAdmin = createClient(
-  process.env.SUPABASE_URL! || 'https://wcqqluhzyzprvmfqsdgh.supabase.co',
-  process.env.SUPABASE_SERVICE_ROLE_KEY! ||
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndjcXFsdWh6eXpwcnZtZnFzZGdoIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2MDY4MDE4MiwiZXhwIjoyMDc2MjU2MTgyfQ.NW0gM5dqCBvQIIYS05dW2c3qSFHmfH1DRj2j4CUxkww',
-);
+export const createClient = async () => {
+  const cookieStore = await cookies();
+
+  return createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        getAll() {
+          return cookieStore.getAll();
+        },
+        setAll(cookiesToSet) {
+          cookiesToSet.forEach(({ name, value, options }) =>
+            cookieStore.set(name, value, options)
+          );
+        },
+      },
+    }
+  );
+};
