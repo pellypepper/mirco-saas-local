81b8e7d5526a1c21ff2140d35c3775971348870a
ppeliance <144333180+ppeliance@users.noreply.github.com>
2026-04-20 20:39:29 +0100

    Update supabaseClient.ts

--- src/libs/supabaseClient.ts
+++ src/libs/supabaseClient.ts
@@ -2,5 +2,10 @@ import { createBrowserClient } from '@supabase/ssr';
 
 export const createClient = () => createBrowserClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+  {
+    auth: {
+      flowType: 'implicit', // ← add this
+    }
+  }
 );
